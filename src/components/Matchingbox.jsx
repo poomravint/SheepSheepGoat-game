1e2f8cfb438667a1dbeed1ffc5a7c073df0f3c41
Pennapa Butda <poomravintwongwaro@gmail.com>
2026-01-23 12:47:26 +0700

    Add Matchingbox

--- src/components/Matchingbox.jsx
+++ src/components/Matchingbox.jsx
@@ -4,26 +4,52 @@ import { getWord } from "./wordPair";
 const Matchingbox = ({ groupname, grouprole, results, setResults }) => {
   const word = getWord();
 
-  const handlematching = () => {
-    console.log(groupname.lenght)
-    console.log(grouprole.lenght)
+  //* Shuffle item function
+  const shuffle = (item) => [...item].sort(() => Math.random() - 0.5);
 
-    if (groupname.lenght === 0 && grouprole.lenght === 0) {
+  const handlematching = () => {
+    if (groupname.length === 0 && grouprole.length === 0) {
       alert("Please add Member and Role");
       return;
     }
 
-    if (groupname.lenght != grouprole.lenght){
+    if (groupname.length != grouprole.length) {
       alert("Member and Role doesn't match");
       return;
     }
 
+    //* Shuffle Role
+    const shuffleRole = shuffle(grouprole);
+
+    //* Shuffle Word
+    const isABlackSheep = Math.random() < 0.5;
+    const wsWord = isABlackSheep ? word.b : word.a;
+    const bsWord = isABlackSheep ? word.a : word.b;
+
+    const matched = groupname.map((name, index) => ({
+      name,
+      role: shuffleRole[index],
+      word:
+        shuffleRole[index] === "WhiteSheep"
+          ? wsWord
+          : shuffleRole[index] === "BlackSheep"
+            ? bsWord
+            : "???",
+    }));
+    setResults(matched);
+    console.log(matched)
+ 
   };
 
   return (
     <>
       <div>
-        <button className="random-button" onClick={handlematching}>Random</button>
+        <p>
+          Member : {groupname.length} Role : {grouprole.length}
+        </p>
+        <button className="random-button" onClick={handlematching}>
+          Random
+        </button>
       </div>
     </>
   );
