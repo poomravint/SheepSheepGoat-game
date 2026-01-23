ecb44f05f1c836aa415e73544d9b984c7b9fa098
Pennapa Butda <poomravintwongwaro@gmail.com>
2026-01-23 07:40:00 +0700

    Add Topic

---
+++ sheepsheepblack-app/src/components/Topic.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Topic.css"
+
+function Topic() {
+  return (
+    <>
+      <div className="topic-content">
+        <div className="topic-white">
+          <h1>SHEEP</h1>
+        </div>
+        <div className="topic-black">
+          <h1>SHEEP</h1>
+        </div>
+        <div className="topic-goat">
+          <h1>GOAT</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Topic;
